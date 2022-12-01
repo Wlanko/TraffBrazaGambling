0659e40c5a2b19bd8d8516c0d3a9e09d123ecafa
Vlad Kyrylenko <sorvigolovaqwerty@gmail.com>
2022-12-01 21:09:14 +0200

    Initial Commit

---
+++ TraffBrazaGambling/TraffBrazaGamblingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraffBrazaGamblingApp.swift
+//  TraffBrazaGambling
+//
+//  Created by Vlad Kyrylenko on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TraffBrazaGamblingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
